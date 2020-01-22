9d9a1169f274bda9760b00b24737807eda520776
NishanW <nishanw@gmail.com>
2020-01-22 19:54:22 +0530

    hello 2020!

--- html-header.php
+++ html-header.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Travelerpic</title>
+    <title>travelerpic.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
